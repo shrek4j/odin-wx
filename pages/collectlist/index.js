8d739959b9df287ac7b96bb6c3e404fb5ee979c5
shrek4j <scxtjwgy2005@163.com>
2017-09-15 19:11:54 +0800

    1

---
+++ pages/collectlist/index.js
@@ -0,0 +1,43 @@
+var base64 = require("../images/base64");
+
+Page({
+  showwordlist: function (e) {
+    var id = e.target.dataset.mid
+    wx.navigateTo({
+      url: '../wordlist/index?morphemeId=' + id
+    });
+  },
+  onLoad: function (options) {
+    var sfz = wx.getStorageSync('sfz')
+
+    var that = this;
+    wx.request({
+      url: 'https://odin.bajiaoshan893.com/CollectRoot/showRootCollectsByUser',
+      data: {
+        'userId': sfz
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        var msg = res.data
+        if (msg == 'noresult') {
+          return;
+        }
+        var dataObj = JSON.parse(msg);
+
+        //--转换translation的<br/>
+        var collectlist = dataObj.collects;
+        var num = 1;
+        for (var i = 0; i < collectlist.length;i++){
+          collectlist[i]['num'] = num++
+        }
+        //--转换translation的<br/>
+
+        that.setData({
+          dataObj: dataObj
+        });
+      }
+    });
+  }
+})
